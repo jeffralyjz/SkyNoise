0f4c6173334e4ab4563355bbcd915bd298a0d194
Jeffrey David Salazar <004626790@coyote.csusb.edu>
2017-06-28 13:18:05 -0700

    Fixed delay function and changed delay_test to be able to test a delay of greater than one

--- delay.py
+++ delay.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 def delay(a, dt=0):
-    rand = np.random.rand(dt)
-    randsh = rand.shape
-    d = np.concatenate((rand, a[randsh[0]:]), axis=0)
-    return d
+    if dt==0:
+        return a
+    elif dt>0:
+        rand = np.random.rand(dt)
+        d = np.concatenate((rand, a[:-rand.shape[0]]), axis=0)
+        return d
--- delay_test.py
+++ delay_test.py
@@ -4,11 +4,15 @@ import numpy as np
 
 class TestDelay(unittest.TestCase):
     def test_delay(self):
-        n1 = np.random.randn(1000)
-        n1d = delay.delay(n1, 0)
-        self.assertTrue(np.all(n1 == n1d))
-        n1d = delay.delay(n1, 1)
-        self.assertTrue(np.all(n1[:-1] == n1d[1:]))
+        n1 = np.random.randn(100)
+        for i in range(99):    
+            n1d=delay.delay(n1,i)
+            if i ==0:
+                self.assertTrue(np.all(n1==n1d))
+            else:
+                self.assertTrue(np.all(n1[:-i]==n1d[i:]))
+                if i == 10:
+                    print np.all(n1[:-i]==n1d[i:])
 
 if __name__ == '__main__':
     unittest.main()
