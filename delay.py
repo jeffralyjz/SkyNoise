cc6eed93cd155bf6d5afeb024ca70de99eec5494
Jeffrey David Salazar <004626790@coyote.csusb.edu>
2017-06-30 13:03:09 -0700

    Fixed bugs in delay.py

--- delay.py
+++ delay.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def delay(a, dt=0):
     if dt==0:
@@ -13,15 +14,14 @@ def delay(a, dt=0):
         d = np.concatenate((rand, a[:-rand.shape[0]]), axis=0)
         return d
 
--------------------------------------------------------------------
 #This function delays the f array by a decimal amount
-def tau(f, tau, graph = 'n')
+def tau(f, tau, graph = 'n'):
     #Fourier transform f
     f_fft=np.fft.fft(f)
     
     #returns cycles per second if sample spacing of f is 
     #in seconds
-    nu = np.fft.fftfreq(f.shape[0})
+    nu = np.fft.fftfreq(f.shape[0])
     
     #Phase shift(kronecker delta)
     phi = np.exp(-2*np.pi*1j*nu*tau)
@@ -32,5 +32,5 @@ def tau(f, tau, graph = 'n')
     if graph =='n':
         return fsh
     elif graph =='y':
-        return plt.plot(fsh, c='r'), plt.plot(f, c='g')
+        return plt.plot(fsh, c='r', label = 'shifted'), plt.plot(f, c='g', label = 'original'), plt.legend(loc=1)
 
