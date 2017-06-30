4220aa316ba1b282333b48466b6852a068facfd3
Jeffrey David Salazar <004626790@coyote.csusb.edu>
2017-06-30 11:47:11 -0700

    Combined delay1.py and delay2.py as delay.py

--- delay.py
+++ delay.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+def delay(a, dt=0):
+    if dt==0:
+        #With delay zero, the array should remain
+        #unchanged.
+        return a
+    elif dt>0:
+        rand = np.random.rand(dt)
+        # d is the shifted original array
+        # ex. if original = [A,B,C] i = 2
+        # then d_original = [rand1, rand2, A]
+        d = np.concatenate((rand, a[:-rand.shape[0]]), axis=0)
+        return d
+import numpy as np
+
 
 #This function delays the f array by a decimal amount
 def tau(f, tau, graph = 'n')
