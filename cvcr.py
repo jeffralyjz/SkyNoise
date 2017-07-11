c8aacf20fe6064ab138752639bcfa889ed3853cb
Jeffrey David Salazar <004626790@coyote.csusb.edu>
2017-07-11 15:36:50 -0700

    Fixed bugs in cvcr.py Fixed bugs in delay and added the mult function

--- cvcr.py
+++ cvcr.py
@@ -1,16 +1,27 @@
 import numpy as np
 
-def cvcr(f,g, cvcr = 'conv'):
+"""Convolves or correlates to arrays.
+op is operation type.
+scv = Return array in original array units(convolution thm)
+scr = return correlation between the two arrays by conjugating the second
+hcv = similiar to scv but ignores inverse fourier transform and returns array in 'Hz'
+hcr = similiar to scr but ignores the inverse fourier transform"""
+
+def cvcr(f,g, op='scv'):
+    assert (op in ('scv','scr','hcv','hcr'))
     #fourier transform functions
     f_fft = np.fft.fft(f)
     g_fft = np.fft.fft(g)
-
-    if cvcr == 'conv':
-        fcvg = np.fft.ifft(f_fft*g_fft)
-        fcvgr = fcvg.real
-        return fcvgr
-    elif cvcr == 'corr':
-        fcrg = np.fft.ifft(f_fft*g_fft.conj())
-        fcrgr = fcrg.real
-        return fcrgr
+    fg = f_fft*g_fft
+    fgc = f_fft*g_fft.conj()
+    if op=='scv':
+        fcvg = np.fft.ifft(fg)
+        return fcvg
+    elif op=='scr':
+        fcrg = np.fft.ifft(fgc)
+        return fcrg
+    elif op=='hcv':
+        return fg
+    elif op=='hcr':
+        return fgc
 
--- delay.py
+++ delay.py
@@ -16,12 +16,12 @@ This mode can handle float type delays
 '''
 def delay(a, dt, mode='wcon'):
     assert (mode in ('wcon' , 'ncon', 'wfft')) #select mode from list
-    assert (0 < dt < a.size) #delay out of possible range
+    assert (0 <= dt < a.size) #delay out of possible range
     if mode =='wcon':
         assert(type(dt) in (int, long))#To do float type delays use wfft
         d_a = np.concatenate((a[dt-a.size:],a[:dt]))
         return d_a
-    elif mode=='nconc':
+    elif mode=='ncon':
         assert(type(dt) in (int, long))#To do float type delays use wfft
         rand = np.random.rand(dt)
         d_a = np.concatenate((rand, a[:-rand.size]), axis=0)
@@ -35,3 +35,27 @@ def delay(a, dt, mode='wcon'):
         fcvp = np.fft.ifft(phi*f_fft)
         d_a = fcvp.astype(a.dtype)
         return d_a
+'''This function serves to create multiple arrays that are delayed by some random amount.
+signals: Number of desired signal arrays
+elem: number of elements in signal array
+ld: lowest delay
+hd: highest delay
+'''
+def mult(signals, elem, tau='n'):
+    assert tau in ('n' , 'rand', 'choose')
+    n = np.random.randn(signals, elem)
+    if tau == 'n':
+        return n
+    if tau == 'rand':
+        ld = input('Enter minimum delay: ')
+        hd = input('Enter max delay: ')
+        for i in range(signals):
+            r = np.random.randint(ld, hd)
+            n[i]= delay(n[i], r, 'wfft')
+        return n
+    if tau =='choose':
+        for i in range(signals):
+            print 'Delay array ' , i+1 , ' by: '
+            r = input()
+            n[i]= delay(n[i], r, 'wfft')
+        return n
